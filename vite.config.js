c8785408237009fb4aee4217452db23a22af4a35
Mahesh21092000 <mekalamahesh1542@gmai.com>
2024-04-20 00:08:23 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+    // Adjusted the base option with the correct repository name
+    build: {
+      outDir: 'dist',
+      assetsDir: '',
+      assetsInlineLimit: 0,
+      sourcemap: false,
+      minify: 'terser', // You can change this to 'esbuild' for faster minification
+    },
+});
